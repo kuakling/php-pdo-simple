1071102d81caaabae8cdb26ad2221bd47c1039d3
Surakit Choodet <kuakling@gmail.com>
2017-08-29 15:47:06 +0700

    Form Validation

--- src/pages/login.php
+++ src/pages/login.php
@@ -12,31 +12,43 @@ body{
 }
 ";
 
-if ($_POST) {
-  $sth = $app['db']->prepare("SELECT * FROM user WHERE username = :username");
-  $sth->execute([
-    'username' => $_POST['username']
-  ]);
-  $result = $sth->fetch(PDO::FETCH_ASSOC);
-  if($result){
-    if (password_verify($_POST['password'], $result['password'])) {
-      $_SESSION['auth'] = [
-        'isAuthenticated' => true,
-        'user' => [
-          'id' => $result['id'],
-          'username' => $result['username'],
-          'email' => $result['email'],
-          'status' => $result['status'],
-          'created_at' => $result['created_at'],
-          'updated_at' => $result['updated_at'],
-        ],
-      ];
-      header('location: index.php');
-    } else {
-      echo 'No Invalid password.';
-    }
+$errors = [];
+if($_POST) {
+  if($_POST['username'] == "") {
+    $errors['username'] = 'กรุณาระบุ Username';
+  }
+  if($_POST['password'] == "") {
+    $errors['password'] = 'กรุณาระบุ Password';
+  }
+
+  if($errors){
+    //กรณีมี Error
   }else{
-    echo 'No user';
+    $sth = $app['db']->prepare("SELECT * FROM user WHERE username = :username");
+    $sth->execute([
+      'username' => $_POST['username']
+    ]);
+    $result = $sth->fetch(PDO::FETCH_ASSOC);
+    if($result){
+      if (password_verify($_POST['password'], $result['password'])) {
+        $_SESSION['auth'] = [
+          'isAuthenticated' => true,
+          'user' => [
+            'id' => $result['id'],
+            'username' => $result['username'],
+            'email' => $result['email'],
+            'status' => $result['status'],
+            'created_at' => $result['created_at'],
+            'updated_at' => $result['updated_at'],
+          ],
+        ];
+        header('location: index.php');
+      } else {
+        echo 'No Invalid password.';
+      }
+    }else{
+      echo 'No user';
+    }
   }
 }
 ?>
@@ -47,12 +59,14 @@ if ($_POST) {
     <div class="card-text">
       <form method="post">
         <div class="form-group">
-          <label for="usernam">Username</label>
-          <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+          <label for="username">Username</label>
+          <input type="text" class="form-control<?php if(isset($errors['username'])){ echo " is-invalid";}?>" id="username" name="username" placeholder="Username">
+          <div class="invalid-feedback"><?php if(isset($errors['username'])){ echo $errors['username'];}?></div>
         </div>
         <div class="form-group">
           <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+          <input type="password" class="form-control<?php if(isset($errors['password'])){ echo " is-invalid";}?>" id="password" name="password" placeholder="Password">
+          <div class="invalid-feedback"><?php if(isset($errors['password'])){ echo $errors['password'];}?></div>
         </div>
         <button type="submit" class="btn btn-primary">Login</button>
       </form>
--- src/pages/signup.php
+++ src/pages/signup.php
@@ -12,34 +12,50 @@ body{
 }
 ";
 
-// $sth = $app['db']->prepare("SELECT * FROM user");
-// $sth->execute();
-// $result = $sth->fetch(PDO::FETCH_ASSOC);
-// print_r($result);
+$errors = [];
 if($_POST) {
-  $options = [
-    'cost' => 11,
-    'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
-  ];
-  $password_hash = password_hash($_POST['password'], PASSWORD_BCRYPT, $options);
-  $stmt = $app['db']->prepare("INSERT INTO user (username, password, email, status, created_at, updated_at) VALUES (:username, :password, :email, :status, :created_at, :updated_at)");
-  $stmt->execute([
-    'username' => $_POST['username'],
-    'password' => $password_hash,
-    'email' => $_POST['email'],
-    'status' => 1,
-    'created_at' => date('Y-m-d h:i:s'),
-    'updated_at' => date('Y-m-d h:i:s'),
-  ]);
+  if($_POST['username'] == "") {
+    $errors['username'] = 'กรุณาระบุ Username';
+  }
+  if($_POST['password'] == "") {
+    $errors['password'] = 'กรุณาระบุ Password';
+  }
+  if($_POST['email'] == "") {
+    $errors['email'] = 'กรุณาระบุ e-Mail';
+  }
 
-  $errorInfo = $stmt->errorInfo();
-  if($errorInfo[2]){
-    $app['flashMessages'][] = [
-      'type' => 'warning',
-      'text' => $errorInfo[2]
-    ];
+  if($errors){
+    // foreach ($errors as $error) {
+    // $app['flashMessages'][] = [
+    //   'type' => 'danger',
+    //   'text' => $error
+    // ];
+    // }
   }else{
-    header('location: index.php');
+    $options = [
+      'cost' => 11,
+      'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+    ];
+    $password_hash = password_hash($_POST['password'], PASSWORD_BCRYPT, $options);
+    $stmt = $app['db']->prepare("INSERT INTO user (username, password, email, status, created_at, updated_at) VALUES (:username, :password, :email, :status, :created_at, :updated_at)");
+    $stmt->execute([
+      'username' => $_POST['username'],
+      'password' => $password_hash,
+      'email' => $_POST['email'],
+      'status' => 1,
+      'created_at' => date('Y-m-d h:i:s'),
+      'updated_at' => date('Y-m-d h:i:s'),
+    ]);
+
+    $errorInfo = $stmt->errorInfo();
+    if($errorInfo[2]){
+      $app['flashMessages'][] = [
+        'type' => 'warning',
+        'text' => $errorInfo[2]
+      ];
+    }else{
+      header('location: index.php');
+    }
   }
 }
 
@@ -52,16 +68,19 @@ if($_POST) {
      <div class="card-text">
        <form method="post">
          <div class="form-group">
-           <label for="usernam">Username</label>
-           <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+           <label for="username">Username</label>
+           <input type="text" class="form-control<?php if(isset($errors['username'])){ echo " is-invalid";}?>" id="username" name="username" placeholder="Username">
+           <div class="invalid-feedback"><?php if(isset($errors['username'])){ echo $errors['username'];}?></div>
          </div>
          <div class="form-group">
            <label for="password">Password</label>
-           <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+           <input type="password" class="form-control<?php if(isset($errors['password'])){ echo " is-invalid";}?>" id="password" name="password" placeholder="Password">
+           <div class="invalid-feedback"><?php if(isset($errors['password'])){ echo $errors['password'];}?></div>
          </div>
          <div class="form-group">
            <label for="email">e-Mail</label>
-           <input type="text" class="form-control" id="email" name="email" placeholder="e-Mail">
+           <input type="text" class="form-control<?php if(isset($errors['email'])){ echo " is-invalid";}?>" id="email" name="email" placeholder="e-Mail">
+           <div class="invalid-feedback"><?php if(isset($errors['email'])){ echo $errors['email'];}?></div>
          </div>
          <button type="submit" class="btn btn-primary">Signup</button>
          <br />

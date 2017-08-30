be2425d8844771326d7366076ab0bb6e56105abc
Surakit Choodet <kuakling@gmail.com>
2017-08-30 21:18:03 +0700

    22 - ปรับคำสั่ง sql ให้แสดงผล id ของตาราง product & การแบ่งหน้า Part 1

--- src/pages/admin/product/index.php
+++ src/pages/admin/product/index.php
@@ -1,8 +1,26 @@
 <?php admin_init(); ?>
 <?php
-$sth = $app['db']->prepare("SELECT * FROM product INNER JOIN product_type ON product.type_id = product_type.id");
-$sth->execute();
-$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+$limit = 5;
+$p = (isset($_GET['p'])) ? intval($_GET['p']) : 1;
+$offset = ($p-1)*$limit;
+$sql['columns'] = " product.*, product_type.type_name";
+$sql['table'] = " FROM product INNER JOIN product_type ON product.type_id = product_type.id";
+$sql['page'] = " LIMIT $limit OFFSET $offset";
+
+$prepare['page'] = "SELECT" . $sql['columns'] . $sql['table'] . $sql['page'];
+//SELECT product.*, product_type.type_name FROM product INNER JOIN product_type ON product.type_id = product_type.id LIMIT 5 OFFSET 0
+
+$prepare['count'] = "SELECT count(*) as count" . $sql['table'];
+// SELECT count(*) as count FROM product INNER JOIN product_type ON product.type_id = product_type.id
+
+$sth['page'] = $app['db']->prepare($prepare['page']);
+$sth['page']->execute();
+$result = $sth['page']->fetchAll(PDO::FETCH_ASSOC);
+
+$sth['count'] = $app['db']->prepare($prepare['count']);
+$sth['count']->execute();
+$rowCount = $sth['count']->fetch(PDO::FETCH_ASSOC)['count'];
+echo $rowCount;
 
 $app['pageTitle'] = "สินค้า";
 ?>
@@ -37,6 +55,26 @@ $app['pageTitle'] = "สินค้า";
     <?php endforeach; ?>
   </tbody>
 </table>
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+        <span class="sr-only">Next</span>
+      </a>
+    </li>
+  </ul>
+</nav>
 
 <?php
 $app['jsScripts'][] = "

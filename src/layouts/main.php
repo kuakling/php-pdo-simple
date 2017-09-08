d1a73d0738a00f5adeff807fa6aad8e8f72dde69
Surakit Choodet <kuakling@gmail.com>
2017-09-08 15:04:06 +0700

    41 - แก้ไขจำนวนสินค้าในตะกร้าด้วย ajax Part 1

--- src/layouts/main.php
+++ src/layouts/main.php
@@ -122,8 +122,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="assets/js/jquery-3.2.1.min.js" ></script>
     <script src="assets/js/popper.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
--- src/pages/cart/index.php
+++ src/pages/cart/index.php
@@ -1,11 +1,11 @@
-<table class="table">
+<table class="table table-striped border">
   <thead class="thead-inverse">
     <tr>
       <th>#</th>
       <th>รหัสสินค้า</th>
       <th>ชื่อสินค้า</th>
       <th class="text-right">ราคา/หน่วย</th>
-      <th class="text-right">จำนวน</th>
+      <th class="text-right" style="width: 150px;">จำนวน</th>
       <th class="text-right">ราคารวม</th>
       <th>...</th>
     </tr>
@@ -36,19 +36,47 @@
       <td><?= $product_id; ?></td>
       <td><?= $product['name']; ?></td>
       <td class="text-right"><?= number_format($product['price']); ?></td>
-      <td class="text-right"><?= $item['amount']; ?></td>
+      <td class="text-right">
+        <form class="update_amount">
+          <div class="form-group">
+            <div class="input-group mb-2 mb-sm-0">
+              <input type="text" class="form-control" id="amount_<?=$product_id?>" name="amount[<?=$product_id?>]" value="<?= $item['amount']; ?>">
+              <button class="btn btn-outline-success my-2 my-sm-0 input-group-addon" type="submit">
+                <i class="fa fa-check" aria-hidden="true"></i>
+              </button>
+            </div>
+        </form>
+      </td>
       <td class="text-right"><?= $price_all; ?></td>
       <td><a href="?page=cart/delete&id=<?=$product_id?>" class="text-danger"><i class="fa fa-trash"></i></a></td>
     </tr>
     <?php endforeach; ?>
+  </tbody>
+  <tfoot class="table-warning" style="font-weight: bold;">
     <tr>
       <td colspan="5" class="text-right">ราคาสุทธิ</td>
       <td class="text-right"><?=number_format($price_total)?></td>
       <td></td>
     </tr>
-  </tbody>
+  </tfoot>
 </table>
 
 <div class="text-right">
   <a href="?page=cart/clear" class="btn btn-outline-danger"><i class="fa fa-trash"></i> ยกเลิกตะกร้าสินค้า</a>
 </div>
+<?php
+$app['jsScripts'][] = "
+$('.update_amount').submit(function(e) {
+  e.preventDefault();
+
+  $.ajax({
+    url: '?page=cart/update',
+    method: 'post',
+    data: $(this).serialize(),
+    success: function(data) {
+      console.log(data);
+    }
+  });
+});
+";
+?>

c46a78e080c807db2a91d072fbe77175f1e1a385
Surakit Choodet <kuakling@gmail.com>
2017-09-09 09:05:03 +0700

    45 - แก้ไขจำนวนสินค้าในตะกร้าด้วย POST

--- src/pages/cart/index_bk.php
+++ src/pages/cart/index_bk.php
@@ -27,6 +27,7 @@
       'id' => $product_id
     ]);
     $product = $sth_product->fetch(PDO::FETCH_ASSOC);
+
     $price_all = $product['price'] * $item['amount'];
     $price_total += $price_all;
     ?>
@@ -34,9 +35,12 @@
       <th scope="row"><?= $i ?></th>
       <td><?= $product_id; ?></td>
       <td><?= $product['name']; ?></td>
-      <td class="text-right"><?= number_format($product['price']); ?></td>
       <td class="text-right">
-        <form class="update_amount" action="?page=cart/update" method="post">
+        <?= number_format($product['price']); ?>
+        <input type="hidden" class="price_<?=$product_id?>" value="<?=$product['price']?>">
+      </td>
+      <td class="text-right">
+        <form class="update_amount">
           <div class="form-group">
             <div class="input-group mb-2 mb-sm-0">
               <input type="text" class="form-control amount_<?=$product_id?>" name="amount[<?=$product_id?>]" value="<?= $item['amount']; ?>">
@@ -46,7 +50,10 @@
             </div>
         </form>
       </td>
-      <td class="text-right"><?= number_format($price_all); ?></td>
+      <td class="text-right">
+        <span class="price_all_display_<?=$product_id?>"><?= number_format($price_all); ?></span>
+        <input type="hidden" class="price_all_<?=$product_id?>" name="price_all[<?=$product_id?>]" value="<?=$price_all?>">
+      </td>
       <td><a href="?page=cart/delete&id=<?=$product_id?>" class="text-danger"><i class="fa fa-trash"></i></a></td>
     </tr>
     <?php endforeach; ?>
@@ -63,3 +70,31 @@
 <div class="text-right">
   <a href="?page=cart/clear" class="btn btn-outline-danger"><i class="fa fa-trash"></i> ยกเลิกตะกร้าสินค้า</a>
 </div>
+<?php
+$app['jsScripts'][] = "
+$('.update_amount').submit(function(e) {
+  e.preventDefault();
+
+  $.ajax({
+    url: '?page=cart/update',
+    method: 'post',
+    data: $(this).serialize(),
+    success: function(data) {
+      console.log(data);
+      var price = $('.price_'+data.product_id).val();
+      var amount = data.amount;
+      var price_all = price*amount;
+      $('.price_all_display_'+data.product_id).text(price_all);
+      $('.price_all_'+data.product_id).val(price_all);
+
+      var items = $('input[name^=\"price_all\"]');
+      var price_total = 0;
+      items.each(function() {
+        price_total += parseInt($(this).val());
+      });
+      $('.price_total').text(price_total);
+    }
+  });
+});
+";
+?>
--- src/pages/cart/update.php
+++ src/pages/cart/update.php
@@ -1,14 +1,23 @@
 <?php
-$app['layout'] = __DIR__ . '/../../layouts/json.php';
-$sess_key = key($_POST['amount']);
-// echo $sess_key;
-$_SESSION['cart'][$sess_key]['amount'] = intval($_POST['amount'][$sess_key]);
+/*******เมื่อต้องการแก้ไขข้อมูลแบบ AJAX ******/
+// $app['layout'] = __DIR__ . '/../../layouts/json.php';
+// $sess_key = key($_POST['amount']);
+// // echo $sess_key;
+// $_SESSION['cart'][$sess_key]['amount'] = intval($_POST['amount'][$sess_key]);
+//
+//
+// $arr = [
+//   'product_id' => $sess_key,
+//   'amount' => $_SESSION['cart'][$sess_key]['amount'],
+// ];
+//
+// echo json_encode($arr);
+?>
 
-// print_r($_SESSION['cart'][$sess_key]);
-$arr = [
-  'product_id' => $sess_key,
-  'amount' => $_SESSION['cart'][$sess_key]['amount'],
-];
+<?php
+/*******เมื่อต้องการแก้ไขข้อมูลแบบ POST ******/
+$product_id = key($_POST['amount']);
+$_SESSION['cart'][$product_id]['amount'] = intval($_POST['amount'][$product_id]);
 
-echo json_encode($arr);
+header("location: ?page=cart/index");
 ?>
